762b9d93c0a8df2023161446d5375170f9e556cb
lzx <lzx@192.168.10.112>
2023-08-17 23:59:52 +0800

    add vue-project

---
+++ VueStudy/vue-project/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" v-model="user.name" />
+    {{ user }}
+    <div></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  user: any;
+  msg: string;
+}>();
+</script>
+
+<style scoped></style>
